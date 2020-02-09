c2d86ac7cb59612f4a78c3cc8e93bb6e4365d285
SS Jones <SSJones.code@gmail.com>
2020-02-09 09:37:48 -0700

    Footer comments on html, comments on js and css as well.

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+//query selectors 
+/*var time9 = document.querySelector("#time9");
+var time10 = document.querySelector("#time10");
+var time11 = document.querySelector("#time11");
+var time12 = document.querySelector("#time12");
+var time1 = document.querySelector("#time1");
+var time2 = document.querySelector("#time2");
+var time3 = document.querySelector("#time3");
+var time4 = document.querySelector("#time4");*/
+
+/*function calendar() {
+    //Time at 9AM
+    var timeat9 = m.format("9 [AM]").toString();
+    time9.append(timeat9);
+  
+    var col9 = document.querySelector("#col9");
+    x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("class", "input9");
+    if (timeat9 == m.format("H [AM]").toString()) {
+      x.setAttribute("placeholder", "Current time");
+      x.style.backgroundColor = "red";
+      col9.append(x);
+    } else if (m.format("H") < m.format("09")) {
+      x.setAttribute("placeholder", "Coming time");
+      x.style.backgroundColor = "green";
+      col9.append(x);
+    } else {
+      x.setAttribute("placeholder", "Passed time");
+      x.style.backgroundColor = "silver";
+      col9.append(x);
+    }*/
+
+
+//text box array
 var textboxArray = $(".form-control");
 for (let i = 0; i < textboxArray.length; i++) {
     let val = getCookie("box" + i);
@@ -10,9 +45,10 @@ for (let i = 0; i < textboxArray.length; i++) {
   function test(box) {
     setCookie("box" + box, textboxArray[box].value);
   }
-  
+
   function setCookie(name, value) {
     document.cookie = name + "=" + value + ";path=/;";
+
   }
    
   function getCookie(name) {
