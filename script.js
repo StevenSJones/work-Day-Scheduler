4a2dafcb5b13103dbe88f29260be768cc4312705
SS Jones <SSJones.code@gmail.com>
2020-02-08 19:51:30 -0700

    get and set cookie function.

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ for (let i = 0; i < textboxArray.length; i++) {
   document.getElementById("date").innerHTML = moment();
    
   console.log(moment())
+  function test(box) {
+    setCookie("box" + box, textboxArray[box].value);
+  }
+  
+  function setCookie(name, value) {
+    document.cookie = name + "=" + value + ";path=/;";
+  }
+   
+  function getCookie(name) {
+    var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+    return v ? v[2] : null;
+  }
+
+
+   
