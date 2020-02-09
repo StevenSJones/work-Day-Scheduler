ee5621a26e4dbe3ec2d3f3844337f9dde2b07e5f
SS Jones <SSJones.code@gmail.com>
2020-02-08 19:46:51 -0700

    git element by id and moment().

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var textboxArray = $(".form-control");
-for (let i = 0; i < textboxArray.length; i++) {}
+for (let i = 0; i < textboxArray.length; i++) {
+    let val = getCookie("box" + i);
+    textboxArray[i].value = val != null ? val : "";
+  }
+   
+  document.getElementById("date").innerHTML = moment();
+   
+  console.log(moment())
